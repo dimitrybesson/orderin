446fcb163c2e9b8a1bb77b775e199263ebfa1967
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-10 16:01:11 -0400

    Filled out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# Item.destroy_all
+Item.destroy_all
 
 items = %w(chicken beef pork tomato potato parsley carrot apple orange tortilla)
 
 items.each { |item| Item.create(name: item, description: 'Food') }
+
+Supplier.create(name: 'Sysco', street_address: '7055 Kennedy Rd, Mississauga, ON, L5S1Y7', phone: '9056708605')
+
+InventoryItem.create(item_id: 3, supplier_id: 1, quantity: 112, price: 59)
+InventoryItem.create(item_id: 6, supplier_id: 1, quantity: 10, price: 121)
+InventoryItem.create(item_id: 7, supplier_id: 1, quantity: 55, price: 154)
+InventoryItem.create(item_id: 1, supplier_id: 1, quantity: 90, price: 199)
+InventoryItem.create(item_id: 2, supplier_id: 1, quantity: 80, price: 170)
+InventoryItem.create(item_id: 4, supplier_id: 1, quantity: 34, price: 12)
+InventoryItem.create(item_id: 8, supplier_id: 1, quantity: 51, price: 345)
