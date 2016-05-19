816ffdbe8d3214052db441b6260d3d1863846fb0
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-19 14:13:50 -0400

    Minor clean up in users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,21 +1,4 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
 $(document).on('ready page:load', function() {
-
-  // $('.restaurant').on('click', function() {
-  //
-  //   var restaurantId = $(this).attr('data-id')
-  //   // console.log(restaurantId)
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: '/suppliers/',
-  //     dataType: 'html',
-  //     data: {restaurant_id: restaurantId},
-  //     success: function(data) {
-  //       $('.user-suppliers').html(data);
-  //     }
-  //   })
-  // })
 
   $('body').on('click', '.restaurant, .supplier', function() {
 
