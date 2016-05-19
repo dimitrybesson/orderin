f22a10a8cf680b85bff8c071d981f77205c2fc03
Joshua Rider <joshrider@protonmail.ch>
2016-05-18 20:20:37 -0400

    supplier show view order modal now disappears upon clickaway

--- app/assets/javascripts/suppliers.js
+++ app/assets/javascripts/suppliers.js
@@ -1,4 +1,10 @@
 $(document).on('ready page:load', function() {
+  $('.supplier-order-modal').on('click', function(e) {
+    e.stopPropagation();
+  });
+  $('.supplier-show-body').on('click', function() {
+    $('.supplier-order-modal').css('display', 'none');
+  });
   $('.supplier-order-button').on('click', function() {
     var supplierIdNumber = $('.supplier-order-button').attr('data');
     var supplierId = {supplierId: supplierIdNumber};
