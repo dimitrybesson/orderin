a5c4c16cd57fd49f5c9736f8767e1a5f12767b8c
Joshua Rider <joshrider@protonmail.ch>
2016-05-19 21:32:52 -0400

    automatically redirects from root to user show if already signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
+
+  before_action :check_signed_in, only: :index
+
   def index
     render layout: false
+  end
+
+  private
+
+  def check_signed_in
+    redirect_to user_path(current_user) if user_signed_in?
   end
 end
