b87e3ca054cfb52095656455df21cc09fff895d2
Dimitry Besson <dimitrybesson@gmail.com>
2016-05-19 23:24:53 -0400

    Added and ran migration to add name column to inventory_item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518004951) do
+ActiveRecord::Schema.define(version: 20160520031801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160518004951) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
   add_index "inventory_items", ["item_id"], name: "index_inventory_items_on_item_id", using: :btree
