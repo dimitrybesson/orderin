9647c8bd315ad1491074d0cda2994af8ffa4953d
Joshua Rider <joshrider@protonmail.ch>
2016-05-25 00:38:06 -0400

    began adding logic to check what people should see on sign in. new users now get options to create a restaurant or supplier.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,22 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+    render partial: 'form'
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      @permission = Permission.new(user_id: current_user.id, role_id: 1, institution_id: @restaurant.id, institution_type: "Restaurant")
+      if @permission.save
+        redirect_to user_url(current_user)
+      else
+        # error handling
+      end
+    else
+      # error handling
+    end
   end
 
   def edit
@@ -20,5 +33,11 @@ class RestaurantsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :street_address, :phone)
+  end
+
 end
