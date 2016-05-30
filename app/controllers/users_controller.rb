30924cd7fe0281838ce954f4532a2b88a22cd2bc
Joshua Rider <joshrider@protonmail.ch>
2016-05-30 01:40:35 -0400

    plugging a minor gap in the supplier orders index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
     elsif @user.supplier_worker?
       @supplier = @user.suppliers.first
       @submitted_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'submitted') and NOT (status ? 'invoiced') and supplier_id = #{@supplier.id}")
-      @invoiced_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'invoiced') and NOT (status ? 'delivered') and supplier_id = #{@supplier.id}")
-      @delivered_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'delivered') and NOT (status ? 'paid') and supplier_id = #{@supplier.id}")
+      @invoiced_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'invoiced') and NOT (status ? 'shipped') and supplier_id = #{@supplier.id}")
+      @shipped_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'shipped') and NOT (status ? 'received') and supplier_id = #{@supplier.id}")
+      @delivered_orders = Order.find_by_sql("SELECT * FROM orders WHERE (status ? 'received') and NOT (status ? 'paid') and supplier_id = #{@supplier.id}")
       render 'supplier_worker_dashboard'
     else
       render 'new_user_dashboard'
